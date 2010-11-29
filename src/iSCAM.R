3c0885b0a2a6d79597d43ee52408260419419955
martell.steve@gmail.com <martell.steve@gmail.com@4d07a6f4-1c50-d4e3-7c70-b4c93817a77e>
2010-11-29 15:28:00 +0000

    R-Code
    git-svn-id: http://iscam-project.googlecode.com/svn/trunk@21 4d07a6f4-1c50-d4e3-7c70-b4c93817a77e

--- src/iSCAM.R
+++ src/iSCAM.R
@@ -67,7 +67,12 @@ repObj	<- read.rep("iscam.rep")
 	#Conditional statements for radio button selections of plots
 	if ( plotType=="catch" )
 	{
-		.plotCatch( repObj, annotate=FALSE )
+		.plotCatch( repObj, legend.txt=NULL )
+	}
+	
+	if ( plotType=="survey" )
+	{
+		.plotIndex( repObj, annotate=FALSE )
 	}
 	
 	if ( plotType=="biomass" )
@@ -79,17 +84,51 @@ repObj	<- read.rep("iscam.rep")
 	{
 		.plotDepletion( repObj, annotate=TRUE )
 	}
+
+	if ( plotType=="mortality" )
+	{
+		.plotMortality( repObj, annotate=TRUE )
+	}
+	
+	if ( plotType=="agecomps" )
+	{
+		.plotAgecomps( repObj )
+	}
 }
 
 
-.plotCatch	<- function( repObj, annotate=FALSE )
+
+
+
+
+.plotIndex	<- function( repObj, annotate=FALSE )
+{
+	#line plot for relative abundance indices
+	with(repObj, {
+		xx=t(iyr)
+		yy=t(it)
+		yrange=c(0, max(yy, na.rm=TRUE))
+		
+		matplot(xx, yy, type="n", axes=FALSE,
+			xlab="Year", ylab="Relative abundance", 
+			ylim=yrange )
+		
+		matlines(xx, yy, col="black",type="o")
+		
+		axis( side=1 )
+		axis( side=2, las=.VIEWLAS )
+		box()
+	})
+}
+
+.plotCatch	<- function( repObj, legend.txt=NULL )
 {
 	#barplot of the observed catch
 	with(repObj, {
 		barplot(obs_ct, names.arg=yr,axes=FALSE, 
-			xlab="Year", ylab="Catch (t)")
+			xlab="Year", ylab="Catch (t)", 
+			legend.text = legend.txt)
 		axis( side=2, las=.VIEWLAS )
-		
 	})
 }
 
@@ -152,7 +191,162 @@ repObj	<- read.rep("iscam.rep")
 			legend( "top",legend=c( "Pre-fishery biomass","Spawning biomass"),
 				bty='n',lty=c(1,2),lwd=c(1,1),pch=c(-1,-1),ncol=1 )
 		}
-	})
-	
+	})	
 }
 
+.plotMortality	<- function( repObj, annotate=FALSE )
+{
+	#plot average total mortality,  fishing mortality & natural mortality
+	with(repObj, {
+		xx=yr
+		yy=t(as.matrix(ft))
+		yrange=c(0, max(yy, na.rm=TRUE))
+		
+		matplot(xx, yy, type="n", axes=FALSE, ylim=yrange, 
+			xlab="Year", ylab="Mortality rate")
+			
+		matlines(xx, yy, col="black")
+		axis( side=1 )
+		axis( side=2, las=.VIEWLAS )
+		box()
+		grid()
+		
+		if ( annotate )
+		{
+			txt = paste("Gear",1:ngear)
+			mfg <- par( "mfg" )
+			if ( mfg[1]==1 && mfg[2]==1 )
+			legend( "top",legend=txt,
+				bty='n',lty=1:ngear,lwd=1,pch=-1,ncol=1)
+		}
+	})
+}
+
+.plotAgecomps	<- function(repObj)
+{
+	#Bubble plot of age-composition data
+	#A is the observed age-comps
+	#Ahat is the predicted age-comps (proportions)
+	with( repObj, {
+		nagear = unique(A[, 2])
+		for(i in nagear)
+		{
+			ac = subset(A, A[, 2]==i)
+			xx = ac[, 1]
+			zz = t(ac[, -1:-2])
+			
+			# plot proportions-at-age (cpro=TRUE)
+			.plotBubbles(zz, xval = xx, yval = age, cpro=TRUE, hide0=TRUE,  
+				las=.VIEWLAS, xlab="Year", ylab="Age")
+		}
+	})
+}
+
+.plotBubbles <- function (z, xval = FALSE, yval = FALSE, dnam = FALSE, rpro = FALSE, 
+    cpro = FALSE, rres = FALSE, cres = FALSE, powr = 1, size = 0.2, 
+    lwd = 1, fg.clrs = c("black", "red", "blue"), bg.clrs = c("white","white","white"), 
+    hide0 = FALSE, debug = FALSE, ...) 
+{
+    dz <- dim(z)
+    ny <- dz[1]
+    nx <- dz[2]
+    xval1 <- 1:nx
+    yval1 <- 1:ny
+    nx1 <- nx
+    ny1 <- ny
+    if (mode(xval) == "logical") {
+        if (xval[1]) {
+            xval1 <- z[1, ]
+            ny1 <- ny - 1
+        }
+    }
+    if (mode(yval) == "logical") {
+        if (yval[1]) {
+            yval1 <- z[, 1]
+            nx1 <- nx - 1
+        }
+    }
+    xind <- (nx - nx1 + 1):nx
+    x2 <- xval1[xind]
+    yind <- (ny - ny1 + 1):ny
+    y2 <- yval1[yind]
+    if ((mode(xval) == "numeric") & (length(xval) == nx1)) 
+        x2 <- xval
+    if ((mode(yval) == "numeric") & (length(yval) == ny1)) 
+        y2 <- yval
+    zz <- array(z[yind, xind], dim = c(length(yind), length(xind)), 
+        dimnames = dimnames(z))
+    if (dnam & !is.null(dimnames(zz))) {
+        if (!is.null(dimnames(zz)[[2]])) {
+            xnam <- as.numeric(dimnames(zz)[[2]])
+            if (!any(is.na(xnam)) && (length(xnam) == 1 || all(diff(xnam) > 
+                0 | all(diff(xnam) < 0)))) 
+                x2 <- xnam
+        }
+        if (!is.null(dimnames(zz)[[1]])) {
+            ynam <- as.numeric(dimnames(zz)[[1]])
+            if (!any(is.na(ynam)) && (length(ynam) == 1 || all(diff(ynam) > 
+                0 | all(diff(ynam) < 0)))) 
+                y2 <- ynam
+        }
+    }
+    xx <- rep(x2, each = length(y2))
+    yy <- rep(y2, length(x2))
+    minz <- min(zz, na.rm = TRUE)
+    maxz <- max(zz, na.rm = TRUE)
+    if (rpro | cpro) {
+        if (minz < 0) {
+            zz <- zz - minz
+            minz <- 0
+            maxz <- max(zz, na.rm = TRUE)
+        }
+    }
+    if (rpro) {
+        zs <- apply(zz, 1, sum, na.rm = TRUE)
+        zz <- sweep(zz, 1, zs, "/")
+    }
+    if (cpro) {
+        zs <- apply(zz, 2, sum, na.rm = TRUE)
+        zz <- sweep(zz, 2, zs, "/")
+    }
+    if (rres) {
+        zm <- apply(zz, 1, mean, na.rm = TRUE)
+        zz <- sweep(zz, 1, zm, "-")
+    }
+    if (cres) {
+        zm <- apply(zz, 2, mean, na.rm = TRUE)
+        zz <- sweep(zz, 2, zm, "-")
+    }
+    zNA <- is.na(zz) | is.nan(zz) | is.infinite(zz)
+    zz[zNA] <- 0
+    z0 <- sign(zz) * abs(zz)^abs(powr)
+    z1 <- z3 <- z0
+    z1[z0 <= 0] <- NA
+    z3[z0 < 0 | z0 > 0] <- NA
+    z2 <- -z0
+    z2[z0 >= 0] <- NA
+    za <- max(z0, na.rm = TRUE)
+    zb <- min(z0, na.rm = TRUE)
+    zM <- max(abs(z0))
+    sz1 <- max(za * size/zM, 0.001)
+    sz2 <- max(-zb * size/zM, 0.001)
+    if (debug) 
+        browser()
+    symbols(xx, yy, circles = as.vector(abs(z0)), inches = size, 
+        fg = 0, ...)
+    if (debug) 
+        browser()
+    if (!hide0 && !all(is.na(z3))) {
+        symbols(xx, yy, circles = as.vector(z3), inches = 0.001, 
+            fg = fg.clrs[1], bg = bg.clrs[3], lwd = lwd, add = TRUE, ...)
+    }
+    if (!all(is.na(z2))) {
+        symbols(xx, yy, circles = as.vector(z2), inches = sz2, 
+            fg = fg.clrs[1], bg = bg.clrs[2], lwd = lwd, add = TRUE, ...)
+    }
+    if (!all(is.na(z1))) {
+        symbols(xx, yy, circles = as.vector(z1), inches = sz1, 
+            fg = fg.clrs[1], bg = bg.clrs[1], lwd = lwd, add = TRUE, ...)
+    }
+    invisible(z0)
+}
