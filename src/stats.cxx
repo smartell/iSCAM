6022ca31cb953cba2ff0e088be75dcf1fe7a3395
martell.steve@gmail.com <martell.steve@gmail.com@4d07a6f4-1c50-d4e3-7c70-b4c93817a77e>
2011-05-06 23:36:43 +0000

    minor change so it should compile with Borland compiler
    git-svn-id: http://iscam-project.googlecode.com/svn/trunk@98 4d07a6f4-1c50-d4e3-7c70-b4c93817a77e

--- src/stats.cxx
+++ src/stats.cxx
@@ -355,7 +355,8 @@ dvector pearson_residuals(long m, dvector obs_p, dvector pred_p)
 		//double neff;		//effective sample size
 		//neff=norm(elem_prod(pred_p,1.-pred_p))/norm2(obs_p-pred_p);
 		dvector var=elem_prod(P,(1.-P))/m;
-		max(var)<=0 ? var=1.: var=var;
+		//max(var)<=0 ? var=1.: var=var;
+		if(max(var)<=0) var=1;
 		dvector r=elem_div(O-P,sqrt(var+0.01/14));
 		if(sum(P)==0) r=0;
 		return(r);
