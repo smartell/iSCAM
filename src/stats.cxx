da94a6960d66615db87a8214003e46d944ac5121
martell.steve@gmail.com <martell.steve@gmail.com@4d07a6f4-1c50-d4e3-7c70-b4c93817a77e>
2011-05-20 03:48:59 +0000

    Some fixes and the multinomial from Vivian
    git-svn-id: http://iscam-project.googlecode.com/svn/trunk@120 4d07a6f4-1c50-d4e3-7c70-b4c93817a77e

--- src/stats.cxx
+++ src/stats.cxx
@@ -273,6 +273,89 @@ dvector rmvlogistic(const dvector& p, const double& tau2, const int& seed)
 	return exp(x)/sum(exp(x));
 }
 
+dvariable multinomial(const dmatrix o, const dvar_matrix& p,dvar_matrix& nu,double& tau2,const double minp)
+{	// returns the negative loglikelihood 
+	/*
+     uses Martell dmvlogistic code for grouping age classes 
+     with observed proportions < minp
+     NB minp must be greater than 0, otherwise algorithm returns 
+     an error if one of the observed proportions is zero.
+     tau2 returns the median absolute standardized residual
+	*/
+	RETURN_ARRAYS_INCREMENT();
+	int i,j,k,n;
+	int a = o.colmin();
+	int A=o.colmax();
+	int t=o.rowmin();
+	int T=o.rowmax();
+	dvector tmptau(1,A*T);	// vector of residuals
+    int Ncount=1;
+    dvariable Nsamp;           // multinomial sample size
+    dvariable proc_err=0.009;   // allow for process error in the pred.age freq...fixed value based on HCAM assessments
+	nu.initialize();
+	dvariable nloglike=0.;
+    //ofstream ofs("check.tmp");
+	for(i=t; i<=T; i++)
+	{	
+		Nsamp=sum(o(i))/(1.+proc_err*sum(o(i)));
+		n=0;
+		dvector oo = o(i)/sum(o(i));
+		dvar_vector pp = p(i)/sum(p(i));
+		
+		//count # of observations greater than minp (2% is a reasonable number)
+		for(j=a;j<=A;j++)
+			if(oo(j) > minp)n++;
+		
+		ivector iiage(1,n);
+		dvector o1(1,n); o1.initialize();
+		dvar_vector p1(1,n); p1.initialize();
+		k=1;
+		for(j=a;j<=A;j++)
+		{
+			if(oo(j)<=minp)
+			{
+				o1(k)+=oo(j);
+				p1(k)+=pp(j);
+			}
+			else
+			{
+				o1(k)+=oo(j);
+				p1(k)+=pp(j);
+				if(k<=n)iiage(k)=j;		//ivector for the grouped residuals
+				if(k<n) k++;
+			}
+		}
+		//assign residuals to nu based on iiage index
+		dvar_vector t1 = elem_div(o1-p1,sqrt(elem_prod(p1,1.-p1)/Nsamp));
+		for(j=1;j<=n;j++)
+		{
+			nu(i)(iiage(j))=t1(j);
+			tmptau(Ncount++)=sqrt(square(value(t1(j))));
+		}
+		
+		/* FIXME  array out of bounds error with this code.
+		//Later addition from Viv to prevent crashes if
+		//min(p1) is very small.
+		if(min(p1)>1e-4)
+		{
+			dvar_vector t1 = elem_div(o1-p1,sqrt(elem_prod(p1,1.-p1)/Nsamp));
+			for(j=1;j<=n;j++)
+			{
+				nu(i)(iiage(j))=t1(j);
+				tmptau(Ncount++)=sqrt(square(value(t1(j))));
+			}
+		}
+		//end of addition.
+		*/
+		nloglike+=sum(-1.*elem_prod(Nsamp*o1,log(p1))+
+					elem_prod(Nsamp*o1,log(o1)));
+	}
+	dvector w=sort(tmptau(1,Ncount-1));
+	tau2=w(int(Ncount/2.)); //mle of the variance 
+	RETURN_ARRAYS_DECREMENT();
+	return(nloglike);
+}
+
 
 //robust normal approximation to the multinomial distribution
 dvariable multifan(const dmatrix oprop,const dvar_matrix& pprop, const int& Nsamp)
