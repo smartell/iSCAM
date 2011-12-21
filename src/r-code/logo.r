3239281577580c2236f3b460e901bff04c85aadc
Steve Martell <martell.steve@gmail.com>
2011-12-21 08:05:46 -0800

    Changes to iscam logo

--- src/r-code/logo.r
+++ src/r-code/logo.r
@@ -21,16 +21,22 @@ par(mfcol=c(1, 1),
 	xaxt="n", yaxt="n", mar=rep(0.25, length=4), oma=rep(0, length=4))
 
 matplot(x, y+ht, lty=1, type="l",col=1:4,  ylim=c(-0.75, 0.75), bty="n", ylab="", xlab="", xlim=c(-10, 0.5*n))
-text(x[1, ]-0.01*n, y[1, ]-.12, c("i", "S", "C", "A", "M"), col=1:4, cex=2, font=2)
-dev.copy2eps(file="iScamLogo.eps", width=6, height=3)
-dev.off()
 
-#png(file="iScamLogo.png", bg="transparent")
-png(file="iScamLogo.png", bg="transparent",width=100, height=50, pointsize=6)
-png(file="iScamLogo.png", bg="transparent",width=100, height=50, pointsize=6)
-par(mfcol=c(1, 1), 
-	xaxt="n", yaxt="n", mar=rep(0.25, length=4), oma=rep(0, length=4))
+id <- c(1:3, 5)
+text(x[1, id]-0.01*n, y[1, id]-.12, c("i", "S", "C", "M"), srt=0, col=1:3, cex=2, font=2)
+text(x[1, 4]-0.01*n, y[1, 4]-.12, c("A"), srt=180, col=4, cex=2, font=2)
 
-matplot(x, y+ht, lty=1, type="l",col=1:4,  ylim=c(-0.75, 0.75), bty="n", ylab="", xlab="", xlim=c(-10, 0.5*n))
-text(x[1, ]-0.01*n, y[1, ]-.12, c("i", "S", "C", "A", "M"), col=1:4, cex=2, font=2)
-dev.off()
+
+dev.copy2eps(file="iscamLogo.eps", width=6, height=3)
+dev.copy2pdf(file="iscamLogo.pdf", width=6, height=3)
+#dev.off()
+#
+##png(file="iScamLogo.png", bg="transparent")
+#png(file="iScamLogo.png", bg="transparent",width=100, height=50, pointsize=6)
+#png(file="iScamLogo.png", bg="transparent",width=100, height=50, pointsize=6)
+#par(mfcol=c(1, 1), 
+#	xaxt="n", yaxt="n", mar=rep(0.25, length=4), oma=rep(0, length=4))
+#
+#matplot(x, y+ht, lty=1, type="l",col=1:4,  ylim=c(-0.75, 0.75), bty="n", ylab="", xlab="", xlim=c(-10, 0.5*n))
+#text(x[1, ]-0.01*n, y[1, ]-.12, c("i", "S", "C", "A", "M"), col=1:4, cex=2, font=2)
+#dev.off()
