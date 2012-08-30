774302214dac9f0ce196955e519c75b55cafd3de
Steve Martell <martell.steve@gmail.com>
2012-08-30 08:44:08 -0700

    A few more plots parsed out of iSCAM.R

--- src/r-code/iSCAM.R
+++ src/r-code/iSCAM.R
@@ -471,7 +471,7 @@ guiView	<- function()
 	
 		if ( plotType=="agecompsresid" )
 		{
-			.plotAgecompresiduals( repObj )
+			.plotAgeCompResiduals( repObj )
 		}
 	
 		if ( plotType=="recruitment" )
@@ -1172,28 +1172,29 @@ guiView	<- function()
 # 	})
 # }
 
-.plotRecruitmentResiduals	<- function( repObj )
-{
-	#Plot the log residuals between the estimated recruits and
-	#those obtained from the recruitment model (delta)
-	with(repObj, {
-		ii = 1:min(age)
-		xx = yr[-ii]
-		yy = delta
-		absmax = abs(max(yy, na.rm=TRUE))
-		if(absmax< 1e-3) absmax=1
-		yrange=c(-absmax, absmax)
-		
-		plot(xx, yy, type="n",  axes=FALSE, ylim=yrange, 
-			xlab="Year", ylab="Recruitment residuals", main=paste(stock))
-			
-		lines(xx, yy, type="h", col="black")
-		axis( side=1 )
-		axis( side=2,  las=.VIEWLAS )
-		box()
-	})
-}
-
+# DEPRECATED
+#.plotRecruitmentResiduals	<- function( repObj )
+#{
+#	#Plot the log residuals between the estimated recruits and
+#	#those obtained from the recruitment model (delta)
+#	with(repObj, {
+#		ii = 1:min(age)
+#		xx = yr[-ii]
+#		yy = delta
+#		absmax = abs(max(yy, na.rm=TRUE))
+#		if(absmax< 1e-3) absmax=1
+#		yrange=c(-absmax, absmax)
+#		
+#		plot(xx, yy, type="n",  axes=FALSE, ylim=yrange, 
+#			xlab="Year", ylab="Recruitment residuals", main=paste(stock))
+#			
+#		lines(xx, yy, type="h", col="black")
+#		axis( side=1 )
+#		axis( side=2,  las=.VIEWLAS )
+#		box()
+#	})
+#}
+#
 # DEPRECATED
 # .plotIndex	<- function( repObj, annotate=FALSE )
 # {
@@ -1455,36 +1456,37 @@ guiView	<- function()
 # 	})
 # }
 
-.plotAgecompresiduals	<- function(repObj)
-{
-	#Bubble plot of age-composition data
-	#A is the observed age-comps
-	#Ahat is the predicted age-comps (proportions)
-	with( repObj, {
-		if(!is.null(repObj$A)){
-			nagear = unique(A[, 2])
-			xrange = range(A[, 1])
-			#par(mfcol=c(length(nagear), 1))
-			j=0
-			for(i in nagear)
-			{
-				j=j+1
-				ac = subset(A_nu, A_nu[, 2]==i)
-				xx = ac[, 1]
-				zz = t(ac[, -1:-2])
-			
-				# plot residuals
-				plotBubbles(zz, xval = xx, yval = age, rres=FALSE, hide0=TRUE,  
-					las=.VIEWLAS, xlab="Year", ylab="Age", frange=0.0, size=0.5*age_tau2[j],
-					bg=colr("white", 0.5), xlim=xrange,main=paste(stock, "Gear", i))
-				title(main=paste("Variance=",round(age_tau2[j],3)),line=-1,cex.main=0.75)
-			}
-			
-		}
-		else{print("There is no age-composition data")}
-	})
-}
-
+# DEPRECATED
+#.plotAgeCompResiduals	<- function(repObj)
+#{
+#	#Bubble plot of age-composition data
+#	#A is the observed age-comps
+#	#Ahat is the predicted age-comps (proportions)
+#	with( repObj, {
+#		if(!is.null(repObj$A)){
+#			nagear = unique(A[, 2])
+#			xrange = range(A[, 1])
+#			#par(mfcol=c(length(nagear), 1))
+#			j=0
+#			for(i in nagear)
+#			{
+#				j=j+1
+#				ac = subset(A_nu, A_nu[, 2]==i)
+#				xx = ac[, 1]
+#				zz = t(ac[, -1:-2])
+#			
+#				# plot residuals
+#				plotBubbles(zz, xval = xx, yval = age, rres=FALSE, hide0=TRUE,  
+#					las=.VIEWLAS, xlab="Year", ylab="Age", frange=0.0, size=0.5*age_tau2[j],
+#					bg=colr("white", 0.5), xlim=xrange,main=paste(stock, "Gear", i))
+#				title(main=paste("Variance=",round(age_tau2[j],3)),line=-1,cex.main=0.75)
+#			}
+#			
+#		}
+#		else{print("There is no age-composition data")}
+#	})
+#}
+#
 .plotPriors	<-function()
 {
 	## This function plots the prior distributions for the parameters
