61a21cdf15a07bd9fdf749fccc8e0a6f8abc7b1c
catarina wor <catarinawor@gmail.com>
2015-06-30 09:58:34 -0700

    first draft of plotdiscards

--- src/R/MSElib/plotMSEDiscards.R
+++ src/R/MSElib/plotMSEDiscards.R
@@ -3,14 +3,69 @@
 #parei aqui
 require(reshape)
 
+.MSEplotWastage <- function(M, no=3, ci=95)
+{
+	df <- as.data.frame(M[[1]]$sublegal.df)
+	df2<- as.data.frame(M[[2]]$rawsublegal.df)
+	
+	if(ci==95){up=df$wt975;low=wf$wt025}
+	if(ci==90){up=df$wt95;low=df$wt05}
+	if(ci==50){up=df$wt75;low=df$wt25}
+
+	cat(".plotSubLegalCatch\n")
+
+	
+	if(.OVERLAY)
+	{
+		
+		p <- ggplot(df,aes(Year,wt50,col=gear)) + geom_line(width=2)
+		p <- p +  geom_ribbon(aes(ymax=up,ymin=low,fill=Scenario),alpha=0.2)
+		p <- p + facet_grid(.~Procedure)
+
+		if(no>0)
+			{
+
+				itx <- sample(1:length(grep("wt",colnames(df2))),no)
+				itxx <- grep("wt",colnames(df2))[itx]
+				new.df <- df2[,c(1:4,itxx)]
+				new.df <- melt(new.df,id=c("Scenario","Procedure","Year", "gear"))
+				
+				p <- p + geom_line(data=new.df,aes_string(x="Year",y='value',col='gear',linetype='variable'))
+				p <- p +   scale_linetype_manual(values=c("dashed","dotted","dotdash","longdash","twodash","1F","F1","4C88C488"))
+
+			}
+	}
+	else
+	{
+		p <- ggplot(df,aes(Year,wt50),col=gear,fill=gear) + geom_line(width=2)
+		p <- p + geom_ribbon(aes(ymax=up, ymin=low,col=gear,fill=gear),alpha=0.2)
+		p <- p + facet_grid(Scenario~Procedure,scales="free")
+
+		if(no>0)
+			{
+
+				itx <- sample(1:length(grep("wt",colnames(df2))),no)
+				itxx <- grep("wt",colnames(df2))[itx]
+				new.df <- df2[,c(1:4,itxx)]
+				new.df <- melt(new.df,id=c("Scenario","Procedure","Year", "gear"))
+				
+				p <- p + geom_line(data=new.df,aes_string(x="Year",y='value',col='gear',linetype='variable'))
+				p <- p +   scale_linetype_manual(values=c("dashed","dotted","dotdash","longdash","twodash","1F","F1","4C88C488"))
+
+			}
+	
+	p <- p + labs(x="Year",y=paste("Wastage",.UNITS))
+	print(p + .THEME)
+}
+
 .MSEplotSubLegal <- function(M, no=3, ci=95)
 {
 	df <- as.data.frame(M[[1]]$sublegal.df)
 	df2<- as.data.frame(M[[2]]$rawsublegal.df)
 	
-	if(ci==95){up=dt975;low=dt025}
-	if(ci==90){up=dt95;low=dt05}
-	if(ci==50){up=dt75;low=dt25}
+	if(ci==95){up=df$dt975;low=df$dt025}
+	if(ci==90){up=df$dt95;low=df$dt05}
+	if(ci==50){up=df$dt75;low=df$dt25}
 
 	cat(".plotSubLegalCatch\n")
 
@@ -31,68 +86,85 @@ require(reshape)
 				new.df <- melt(new.df,id=c("Scenario","Procedure","Year", "gear"))
 				
 				p <- p + geom_line(data=new.df,aes_string(x="Year",y='value',col='gear',linetype='variable'))
+				p <- p +   scale_linetype_manual(values=c("dashed","dotted","dotdash","longdash","twodash","1F","F1","4C88C488"))
 
 			}
-
 	}
 	else
 	{
-		p <- ggplot(df,aes(Year,p.Bt0.5),col=Scenario,fill=Scenario) + geom_line(width=2)
-		p <- p + geom_ribbon(aes(ymax=up, ymin=low,col=Scenario,fill=Scenario),alpha=0.2)
+		p <- ggplot(df,aes(Year,dt50),col=gear,fill=gear) + geom_line(width=2)
+		p <- p + geom_ribbon(aes(ymax=up, ymin=low,col=gear,fill=gear),alpha=0.2)
 		p <- p + facet_grid(Scenario~Procedure,scales="free")
-	}
-	# p <- p + geom_line(data=bt,aes(Year,Bo),col="blue")
-	p <- p + labs(x="Year",y=paste("Spawning biomass",.UNITS))
+
+		if(no>0)
+			{
+
+				itx <- sample(1:length(grep("dt",colnames(df2))),no)
+				itxx <- grep("dt",colnames(df2))[itx]
+				new.df <- df2[,c(1:4,itxx)]
+				new.df <- melt(new.df,id=c("Scenario","Procedure","Year", "gear"))
+				
+				p <- p + geom_line(data=new.df,aes_string(x="Year",y='value',col='gear',linetype='variable'))
+				p <- p +   scale_linetype_manual(values=c("dashed","dotted","dotdash","longdash","twodash","1F","F1","4C88C488"))
+
+			}
+	
+	p <- p + labs(x="Year",y=paste("SubLegal Discards",.UNITS))
 	print(p + .THEME)
 }
 
-
-.MSEplotMilkaSpawnBiomass <- function(M, no=3, ci=95)
+.MSEplotEfficiency <- function(M, no=3, ci=95)
 {
-	df <- as.data.frame(M[[1]]$biomass.df)
-	df2<- as.data.frame(M[[2]]$rawbiomass.df)
+	df <- as.data.frame(M[[1]]$sublegal.df)
+	df2<- as.data.frame(M[[2]]$rawsublegal.df)
 	
-	cat(".plotSpawnBiomass\n")
+	if(ci==95){up=df$ef975;low=df$ef025}
+	if(ci==90){up=df$ef95;low=df$ef05}
+	if(ci==50){up=df$ef75;low=df$ef25}
 
-	if(ci==95){up=df$t.Bt0.975;low=df$t.Bt0.025}
-	if(ci==90){up=df$t.Bt0.95;low=df$t.Bt0.05}
-	if(ci==50){up=df$t.Bt0.75;low=df$t.Bt0.25}
+	cat(".plotSubLegalCatch\n")
+
 	
 	if(.OVERLAY)
 	{
 		
-		p <- ggplot(df,aes(Year,t.Bt0.5,col=Scenario)) + geom_line(width=2)
-		p <- p +  geom_ribbon(aes(ymax=up, ymin=low,fill=Scenario),alpha=0.2)
+		p <- ggplot(df,aes(Year,ef50,col=gear)) + geom_line(width=2)
+		p <- p +  geom_ribbon(aes(ymax=up,ymin=low,fill=Scenario),alpha=0.2)
 		p <- p + facet_grid(.~Procedure)
 
 		if(no>0)
 			{
-				itx <- sample(1:length(grep("m_sbt",colnames(df2))),no)
-				itxx <- grep("m_sbt",colnames(df2))[itx]
-				new.df <- df2[,c(1:3,itxx)]
-				new.df <- melt(new.df,id=c("Scenario","Procedure","Year"))
-				
-				p <- p + geom_line(data=new.df,aes_string(x="Year",y='value',col='variable'))
-			}
 
+				itx <- sample(1:length(grep("ef",colnames(df2))),no)
+				itxx <- grep("ef",colnames(df2))[itx]
+				new.df <- df2[,c(1:4,itxx)]
+				new.df <- melt(new.df,id=c("Scenario","Procedure","Year", "gear"))
+				
+				p <- p + geom_line(data=new.df,aes_string(x="Year",y='value',col='gear',linetype='variable'))
+				p <- p +   scale_linetype_manual(values=c("dashed","dotted","dotdash","longdash","twodash","1F","F1","4C88C488"))
+
+			}
 	}
 	else
 	{
-		p <- ggplot(df,aes(Year,m.Bt0.5),col=Scenario,fill=Scenario) + geom_line(width=2)
-		p <- p + geom_ribbon(aes(ymax=up, ymin=low,col=Scenario,fill=Scenario),alpha=0.2)
+		p <- ggplot(df,aes(Year,ef50),col=gear,fill=gear) + geom_line(width=2)
+		p <- p + geom_ribbon(aes(ymax=up, ymin=low,col=gear,fill=gear),alpha=0.2)
 		p <- p + facet_grid(Scenario~Procedure,scales="free")
 
 		if(no>0)
 			{
-				itx <- sample(1:length(grep("m_sbt",colnames(df2))),no)
-				itxx <- grep("m_sbt",colnames(df2))[itx]
-				new.df <- df2[,c(1:3,itxx)]
-				new.df <- melt(new.df,id=c("Scenario","Procedure","Year"))
+
+				itx <- sample(1:length(grep("ef",colnames(df2))),no)
+				itxx <- grep("ef",colnames(df2))[itx]
+				new.df <- df2[,c(1:4,itxx)]
+				new.df <- melt(new.df,id=c("Scenario","Procedure","Year", "gear"))
 				
-				p <- p + geom_line(data=new.df,aes_string(x="Year",y='value',col='variable'))
+				p <- p + geom_line(data=new.df,aes_string(x="Year",y='value',col='gear',linetype='variable'))
+				p <- p +  scale_linetype_manual(values=c("dashed","dotted","dotdash","longdash","twodash","1F","F1","4C88C488"))
+
 			}
-	}
-	# p <- p + geom_line(data=bt,aes(Year,Bo),col="blue")
-	p <- p + labs(x="Year",y=paste("Spawning biomass",.UNITS))
+	
+	p <- p + labs(x="Year",y=paste("Efficiency",.UNITS))
 	print(p + .THEME)
 }
+
