bcfb5bac908b13901eafe223d0dab458eff429c5
catarina wor <catarinawor@gmail.com>
2015-06-29 17:01:04 -0700

    improved AAV plots

--- src/R/MSElib/plotMSEAAV.R
+++ src/R/MSElib/plotMSEAAV.R
@@ -16,18 +16,19 @@ require(reshape)
 	#if(ci==90){up=df$ct95;low=df$ct05}
 	#if(ci==50){up=df$ct75;low=df$ct25}
 
-	df22<-melt(df2,id=c("Scenario","Procedure","Year"))
+	df22<-melt(df2,id=c("Scenario","Procedure","Year","gear"))
 	
 	if(.OVERLAY)
 	{	
 
-		p <- ggplot(df22,aes(as.factor(Year),value,col=Scenario)) + geom_boxplot()
+		p <- ggplot(df22,aes(as.factor(Year),value,col=as.factor(gear))) + geom_boxplot(aes(fill=Scenario), alpha=0.2)
 		p <- p + facet_grid(.~Procedure)
 	}
 	else
 	{
-		p <- ggplot(df22,aes(as.factor(Year),value,col=Scenario)) + geom_boxplot()
+		p <- ggplot(df22,aes(as.factor(Year),value,col=as.factor(gear))) + geom_boxplot()
 		p <- p + facet_grid(Scenario~Procedure,scales="free")
+		p
 	}
 	# p <- p + geom_line(data=bt,aes(Year,Bo),col="blue")
 	p <- p + labs(x="Year",y=paste("Annual Absolute Variation in catch",.UNITS))
